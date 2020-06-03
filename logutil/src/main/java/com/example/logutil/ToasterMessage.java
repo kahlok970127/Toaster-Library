6d64550aabecf64f962ad5b55b3992d3a21fa804
kahlok970127 <kahlokinternship@gmail.com>
2020-06-03 12:15:26 +0800

    Test

--- logutil/src/main/java/com/example/logutil/ToasterMessage.java
+++ logutil/src/main/java/com/example/logutil/ToasterMessage.java
@@ -5,9 +5,30 @@ import android.widget.Toast;
 
 public class ToasterMessage {
 
-    public static void s(Context c, String message){
+//    public static void s(Context c, String message){
+//
+//        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+//
+//    }
 
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    public float x,y;
 
+    public ToasterMessage(ToasterMessage vec){
+        this.x=vec.x;
+        this.y=vec.y;
     }
+
+    public ToasterMessage(float x,float y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+    public  ToasterMessage copy(){
+        return new ToasterMessage(this);
+    }
+
+    public void debug(){
+        System.out.println("{"+x+","+y+"}");
+    }
+
 }
